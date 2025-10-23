039f898255f5ffffe90ba24c3fa2f6cd95adc283
Shinovon <arman200711@gmail.com>
2025-10-23 06:39:57 +0500

    Cleanup

--- src/shinovon/unalw1/ALWClassAdapter.java
+++ src/shinovon/unalw1/ALWClassAdapter.java
@@ -18,11 +18,6 @@ public class ALWClassAdapter extends ClassVisitor {
 
 	public ALWClassAdapter(ClassVisitor visitor, String name) {
 		super(Opcodes.ASM4, visitor);
-		if (("auto".equals(Main.inst.mode) || "vserv".equals(Main.inst.mode))
-				&& name.endsWith("VservManager")) {
-			// TODO: vserv: check if all vserv have this class
-			Main.inst.log("Found VservManager");
-		}
 		this.className = name;
 	}
 	
