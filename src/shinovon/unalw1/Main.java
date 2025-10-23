b5661fb0b1dbc17e14cd5379f740243434db7493
Shinovon <arman200711@gmail.com>
2025-10-23 06:37:15 +0500

    Bump version

--- src/shinovon/unalw1/Main.java
+++ src/shinovon/unalw1/Main.java
@@ -57,7 +57,7 @@ import javax.swing.DefaultComboBoxModel;
 
 public class Main implements Runnable {
 	
-	public static final String VERSION = "5.1";
+	public static final String VERSION = "5.2";
 	
 	public static final String[] modes = {
 			"auto",
